a500ffb5bf3ce935cae12e187924f5fcb8b967e4
José Nieto <jdnv.123@gmail.com>
2017-10-28 18:45:35 -0400

    Send messages to user from profile page

--- imports/ui/pages/user.js
+++ imports/ui/pages/user.js
@@ -3,24 +3,33 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import { Files } from '/imports/lib/core.js';
 import { Workshops } from '/imports/api/workshops.js';
+
 import '../components/workshopCard.js';
 import './user.html';
 
 Template.user.onCreated(function() {
   Meteor.subscribe('users');
   Meteor.subscribe('workshops');
+
+  this.user = new ReactiveVar({});
+
+  this.getParam = () => FlowRouter.getParam('_id');
+
+  this.autorun(() => {
+    this.user.set(Meteor.users.findOne(this.getParam()));
+  });
 });
 
 Template.user.helpers({
   user() {
-    const user = Meteor.users.findOne(FlowRouter.getParam('_id'));
+    const user = Template.instance().user.get();
     if(user) {
       document.title = user.profile.name + " | Meetin";
     }
     return user;
   },
-  isUser(user) {
-    return Meteor.userId() === user._id;
+  isCurrentUser() {
+    return Meteor.userId() === Template.instance().user.get()._id;
   },
   picSrc(picId) {
     if(picId) {
@@ -42,5 +51,31 @@ Template.user.helpers({
         $in : attendsTo
       }
     }).fetch();
+  }
+});
+
+Template.user.events({
+  'click .ui.email.organizer.button'() {
+    $('.ui.email.form').transition('slide down');
+  },
+
+  // send email to workshop organizer
+  'click .ui.send.email.button'(e, instance) {
+    const message = $('#emailToUser').val();
+
+    if(Meteor.user()) {
+      Meteor.call('email.toUser', FlowRouter.getParam('_id'), message, (error, result) => {
+        if(error) {
+          alert(error.message);
+        } else {
+          $('.ui.form').transition('slide down');
+          $('.ui.success.message').transition('slide down');
+        }
+      });
+    }
+  },
+
+  'click .ui.settings.button'() {
+    FlowRouter.go('/settings');
   }
 });
